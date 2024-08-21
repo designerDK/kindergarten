2b2bad03f5455ab9087c6a5628c43cc23d89e855
designerDK <tupacalypse@naver.com>
2024-08-21 23:03:24 +0900

    1.0.1

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 let guName = 
 ['강서구', '강남구', '강동구', '광진구']
 
+let clickedGuName;
+
+
+// 구 버튼을 생성하고 눌렀을 때 서치리스트 기능을 실행
 guName.forEach((item)=>{
     console.log(item);
     const guBTN = `
         <button id="gu-btn" onClick="
-            console.log(this.textContent)
+            clickedGuName = this.textContent;
+            console.log(clickedGuName);
+            searchList();
         ">${item}</button>
     `;
     document.querySelector(".states-btn-container")
       .insertAdjacentHTML("beforeend", guBTN);
 });
 
+// 구 버튼 눌렀을 때 서치해서 목록을 생성해주는 기능
+function searchList(){
+  fetch("./json/kinderGeneral.json")
+  .then((response) => response.json())
+  .then((data) => {
 
+    for(let i = 0; i < data.DATA.length; i++){
+      if(data.DATA[i].addr.includes(clickedGuName)){
+        console.log(data.DATA[i].addr)
+        // 리스트페이지로 이동
+        // 리스트페이지의 항목에 넣어주기
+      }else{}
+    }
 
-
-
+  });
+}
